6d36ecb5deec7ab4b2099176f031c2a2e2a8d3a7
Erdem <harmancioglue@gmail.com>
2018-12-01 18:03:39 +0300

    config is updated

--- app/config/db_config.php
+++ app/config/db_config.php
@@ -5,15 +5,10 @@
 
 //DB Params
 
-//define('DB_HOST','localhost');
-//define('DB_USER','__YOUR_USER__');
-//define('DB_PASS','__YOUR_PASS__');
-//define('DB_NAME','__YOUR__DBNAME__');
-
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','myproject');
+define('DB_USER','__YOUR_USER__');
+define('DB_PASS','__YOUR_PASS__');
+define('DB_NAME','__YOUR__DBNAME__');
 
 //App Root
 define('APP_ROUTE',dirname(dirname(__FILE__)));
